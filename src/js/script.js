81e4ba15c4c075adae32c393c946ce761e136e9a
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-26 00:25:05 +0200

    delete class active

--- src/js/script.js
+++ src/js/script.js
@@ -166,7 +166,7 @@
           /* START IF: if option is selected and option is not default */
           if (optionSelected && !option.default) {
             /* add price of option to variable price */
-            price = option.price + price;
+            price = price + option.price;
 
             /* END IF: if option is selected and option is not default */
             /* START ELSE IF: if option is not selected and option is default */
