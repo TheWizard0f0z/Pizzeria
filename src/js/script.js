29041150686503dc620891441c6b78d1f4acdf21
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-28 00:40:27 +0200

    add reaction to events fix

--- src/js/script.js
+++ src/js/script.js
@@ -214,6 +214,7 @@
 
       thisWidget.getElements(element);
       thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
 
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
