e6bb36cf4c2980dc61188c1a9a07743bc435ecc7
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-28 20:20:54 +0200

    Listening for an event

--- src/js/script.js
+++ src/js/script.js
@@ -197,6 +197,9 @@
         /* END LOOP: for each optionId in param.options */
       }
       /* END LOOP: for each paramId in thisProduct.data.params */
+      /* multiply price by amount */
+      price *= thisProduct.amountWidget.value;
+
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       thisProduct.priceElem.innerHTML = price;
     }
@@ -205,6 +208,10 @@
       const thisProduct = this;
 
       thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+
+      thisProduct.amountWidgetElem.addEventListener('updated', function() {
+        thisProduct.processOrder();
+      });
     }
   }
 
@@ -237,6 +244,7 @@
       /* TODO: Add validation */
 
       thisWidget.value = newValue;
+      thisWidget.announce();
       thisWidget.input.value = thisWidget.value;
     }
 
@@ -259,7 +267,7 @@
     announce() {
       const thisWidget = this;
 
-      const event = new event('updated');
+      const event = new Event('updated');
       thisWidget.element.dispatchEvent(event);
     }
   }
