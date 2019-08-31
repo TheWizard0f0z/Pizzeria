dfae9f7646f1b501e65dfa28bee967748f3d5abe
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-31 14:54:12 +0200

    Add new class and instance

--- src/js/script.js
+++ src/js/script.js
@@ -297,6 +297,26 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      console.log('new Cart:', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+  }
+
   const app = {
     initMenu: function() {
       const thisApp = this;
@@ -312,6 +332,13 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function() {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function() {
       const thisApp = this;
       //console.log('*** App starting ***');
@@ -323,6 +350,8 @@
       thisApp.initData();
 
       thisApp.initMenu();
+
+      thisApp.initCart();
     }
   };
 
